ea7c58f50a417e4b22e8eb587a8da9e478ff8a9b
AL3404 <cedfou@gmail.com>
2023-11-14 14:30:17 +0100

    Initial Commit

---
+++ MyWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyWidget
+//
+//  Created by Cedric Fourneau on 14/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyWidget/MyWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWidgetApp.swift
+//  MyWidget
+//
+//  Created by Cedric Fourneau on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
